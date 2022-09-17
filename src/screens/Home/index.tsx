07367511dab850b398d2e1deb9c5c10cf7078f1e
Reverson Thaiam <reverson.santos@frotabank.com.br>
2022-09-16 23:43:03 -0300

    app finished

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,12 +1,31 @@
-import React from 'react';
-import { View } from 'react-native';
-
-import { styles } from './styles';
+import React from 'react'
+import { FlatList, Image, View } from 'react-native'
+import logoImg from '../../assets/logo-nlw-esports.png'
+import { GameCard } from '../../components/GameCard'
+import { Header } from '../../components/Header'
+import { styles } from './styles'
+import { GAMES } from '../../utils/games'
 
 export function Home() {
   return (
     <View style={styles.container}>
+      <Image source={logoImg} style={styles.logo} />
 
+      <Header
+        title="Encontre seu duo!"
+        subtitle="Selecione o game que voce deseja jogar..."
+      />
+
+      <FlatList
+
+        data={GAMES}
+        keyExtractor={(item) => item.id}
+        renderItem={({item}) => <GameCard data={item} />}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.contentList}
+
+      />
     </View>
-  );
+  )
 }
